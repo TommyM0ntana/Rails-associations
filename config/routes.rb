ef3cf657a4be29ffd396ff7c298c79d4e01d171b
Tommym0ntana <tommy.montana.harwood@gmail.com>
2020-01-27 00:10:19 +0100

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
-
+  
   get 'static_page/home'
   get 'static_page/help'
   get 'static_page/about'
+  get  '/help', to: 'static_page#help'
+  get  '/home', to: 'static_page#home'
+  get  'static_page/home', to: 'static_page#home'
+
+  root 'static_pages#home'
+
+
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
